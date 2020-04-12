74695bb066b8152df12b26409e9a4f6dc4080ee5
S Pourtaymour <sepand_pourteymour@live.co.uk>
2020-04-13 00:05:43 +0100

    Added syntax

--- libs/stripeFunctions.js
+++ libs/stripeFunctions.js
@@ -33,7 +33,7 @@ exports.create_ephemeral = async(payload)=>{
   return await new Promise(async(resolve, reject)=>{
     try {
       console.log("Payload", payload);
-      libs.ephemeralKeys.create({customer: req.body.customerId}, {stripe_version: stripe_version})
+      stripe.ephemeralKeys.create({customer: req.body.customerId}, {stripe_version: stripe_version})
       .then((key) => {
           response.status(200).send(key);
       }).catch((err) => {
